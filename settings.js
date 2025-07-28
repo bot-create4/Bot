c058c0ca324d18f1f033227c1d69ee34218d2953
Ransara Devnath (unofficial) <amransaramax@gmail.com>
2025-07-28 20:53:46 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "SUBZERO~b170e2a6d5472ffff31f251017e124896799ba62", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "94726880784", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
 
